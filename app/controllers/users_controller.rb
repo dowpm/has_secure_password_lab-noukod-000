ee50002bb40a5d34fa4211530f65c4fef786e752
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-16 19:09:22 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to controller: 'sessions', action: :new
     else
-      redirect_to controller: 'users', action: :new
+      redirect_to controller: 'users', action: :new, alert: 'Failed creating your account'
     end
   end
 
