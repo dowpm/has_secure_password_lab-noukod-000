8144a1ffbebbd84fb8dc7bf32f619d3a2c931b94
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-16 17:19:25 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def userS_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
