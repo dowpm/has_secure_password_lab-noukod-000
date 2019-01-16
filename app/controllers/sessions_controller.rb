a8f324cec7dfee0cfbec5abac913d354f71db6ae
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-16 19:44:24 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.find_by(params[:name])
+    if @user = User.find_by(params[:user][:name])
       return head(:forbidden) unless @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to controller: 'users', action: :index
